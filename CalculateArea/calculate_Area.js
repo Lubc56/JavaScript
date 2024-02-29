1657e6957cc8422059c1d805a55df73db73f9a09
Luigi <luigi.bulla@gmail.com>
2024-02-29 15:39:53 -0500

    add calculateAmount

--- CalculateArea/calculate_Area.js
+++ CalculateArea/calculate_Area.js
@@ -9,3 +9,11 @@ function calculateArea() {
    document.getElementById('result').innerText = `The area of the rectangle is: ${area}`;
 }
 
+function calculateAmount() {
+    grocery1 = parseFloat(document.getElementById('grocery-1').value);
+    grocery2 = parseFloat(document.getElementById('grocery-2').value);
+    grocery3 = parseFloat(document.getElementById('grocery-3').value);
+   
+   let sum = grocery1 + grocery2 + grocery3;
+   document.getElementById('resultAmount').innerText = `The total amount is :$ ${sum}`;
+}
